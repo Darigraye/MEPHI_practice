07b2542be94059bbd7f3a487bf02a3878ff752d6
Евгений Шпачков <eugene.shpachkov@gmail.com>
2023-09-27 20:53:20 +0300

    create main application

---
+++ annotatesystem/annotate_application/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnnotateApplicationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'annotate_application'
--- annotatesystem/annotatesystem/settings.py
+++ annotatesystem/annotatesystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'annotate_application.apps.AnnotateApplicationConfig'
 ]
 
 MIDDLEWARE = [
