81800d75e32b5fa5e24e1e8162eb6a83da3beda7
Евгений Шпачков <eugene.shpachkov@gmail.com>
2023-10-01 17:39:37 +0300

    change settings for connect to postgresql

--- annotatesystem/annotatesystem/settings.py
+++ annotatesystem/annotatesystem/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'annotatesystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'mephi_database',
+        'USER': 'mephi_app',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
